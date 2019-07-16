79a5eca6c19977fc58f53a5d5de30b96742ee2bb
a-lemon <52976682+a-lemon@users.noreply.github.com>
2019-07-16 15:23:24 -0400

    Add files via upload

---
+++ abtme.js
@@ -0,0 +1,11 @@
+
+function toggleSection(id) {
+  //first get element specified by name
+  var elt= document.getElementById(id
+
+  //if the element in hidden show
+  if (x.stylevisability === ('hidden'){
+    x.stylevisability = 'visible';
+  } else {
+      elt.style.visability = 'hidden'
+  }
